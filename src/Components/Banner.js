2a74059d71470f28d31b6f82e9b5be6d68abe493
Aguspepee <sanchezagustinezequiel@gmail.com>
2021-12-22 18:22:47 -0300

    Banner responsive

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,29 +1,34 @@
-import g869 from '../Images/banner.svg';
+import imagen from '../Images/banner/imagen.svg';
+import texto from '../Images/banner/texto.svg';
+import lateral from '../Images/banner/lateral.svg';
 import './Banner.css';
 
 
 function Banner() {
   return (
-    <div >
-
-      <div className="carousel">
+    <div className='grid'>
+      <div>
         <img
-          className="d-block w-100"
-          src={g869}
+          className="texto"
+          src={texto}
           alt="First slide"
         />
-
       </div>
-      
-
-
-
-
+      <div>
+        <img
+          className="imagen"
+          src={imagen}
+          alt="First slide"
+        />
+      </div>
+      <div className="lateral">
+        <img
+          
+          src={lateral}
+          alt="First slide"
+        />
+      </div>
     </div>
-
-
-
-
   );
 };
 
